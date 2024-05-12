ffebce4706454282157036b6a73438577a00686d
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-05-12 15:34:21 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+using ebooks_dotnet7_api;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("ebooks"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+var app = builder.Build();
+
+var ebooks = app.MapGroup("api/ebook");
+
+// TODO: Add more routes
+ebooks.MapPost("/", CreateEBookAsync);
+
+app.Run();
+
+// TODO: Add more methods
+async Task<IResult> CreateEBookAsync(DataContext context)
+{
+    return Results.Ok();
+}
